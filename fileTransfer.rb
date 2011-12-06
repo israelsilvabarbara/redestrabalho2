7cff495431ff872b64537e975d273924fb71c9a6
israelsilvabarbara <israelsilvabarbara@gmail.com>
2011-12-05 23:31:37 -0200

    adicionando arquivos com criptografia simetrica + privada/publica + certificaçao + hash md5 , testado online e local

--- fileTransfer.rb
+++ fileTransfer.rb
@@ -1,4 +1,6 @@
 
+
+require 'digest/md5'
 require 'socket'
 require 'openssl'
 require 'digest/sha1'
@@ -8,7 +10,7 @@ require 'digest/sha1'
 #	Trabalho2 -- File transfer --
 #	--
 #	-- Transferir um arquiro com segurança e confiabilidade 
-#	-- Israel Silva Barbará
+#       -- Israel Silva Barbará e  Paulo Eugenio Ferreira Fernandes  
 #
 
 
@@ -29,7 +31,6 @@ end
 
 
 def  fileProcess(request )
-
 strfile  = ""
 resposta = ""
 request_part = request.split(",")
@@ -38,7 +39,6 @@ idpedido = request_part[0]
 	if (!verifyDir( caminho )) then 			# verifica se o caminho para o arquivo é possivel
 		resposta = " -- Bad File Request -- "
 	else
-		puts caminho
 		if(File.exist?(caminho)) then				# testa a existencia do arquivo
 			puts "open the file"			
 			myfile  = File.open( caminho )			# Abre o arquivo
@@ -48,6 +48,9 @@ idpedido = request_part[0]
 			resposta = " 404 File Not Found "	
 		end		
 	end
+
+## Cria hash md5 para o texto
+hashtext = Digest::MD5.hexdigest(strfile)
 
 ## Aplica criptografia Simetrica aes (256)
 
@@ -59,13 +62,12 @@ crypt.iv = iv = crypt.random_iv
 
 resposta = crypt.update(strfile)						# criptografa a mensagem com a chave key
 resposta << crypt.final
-puts "encrypted: #{resposta}\n"
 
+resposta = resposta + "|||||" + hashtext
 resposta =  iv + "|||||" + resposta					# concatena chave e iv a resposta
-
 ##
 ## Criptografia de chave publica /privada
-puts "a resposta ate agora ta assim --\n#{resposta}\n"
+
 resposta = cryptPPV( key,resposta,idpedido)
 
 return resposta 
@@ -74,36 +76,18 @@ end
 
 
 def cryptPPV( key,resposta , idpedido)
-	
-	puts idpedido
 
 	pedidoPF    = "./" + idpedido.chomp + "_new_public.pem"				#abre public key so pedido	
-	puts pedidoPF
 	public_key  = OpenSSL::PKey::RSA.new(File.read(pedidoPF))
 	cipher_key = public_key.public_encrypt( key )
 	resposta = cipher_key + "|||||" + resposta
-	puts "chave criptografada com a chave publica pedido: \n"	
-	puts cipher_key
-	puts "####################### Gambiarra ###############################3"
-		#spedidoPF    = "./" + idpedido.chomp + "_new_private.pem"				#abre public key so pedido	
-		#sprivate_key  = OpenSSL::PKey::RSA.new(File.read(spedidoPF))
-	#puts "\n\n\n\n"
 	
-	#puts  sprivate_key.methods	
-	#key = sprivate_key.private_decrypt(cipher_key)
-	#puts "--------------------------------- " + key
-
-	puts "#######################TESTE###############################3"
-
 	# CRIPTO O ID COM A CHAVE PRIVADA TUA
 	private_file = "./" + $id.chomp + "_new_private.pem"
 	private_key  = OpenSSL::PKey::RSA.new(File.read(private_file)) 
 	chipher_id   = private_key.private_encrypt($id)					#crypt ,chave privada na assinatura	
-		
-	
-	
+			
 	resposta = chipher_id + "|||||" + resposta	
-	puts resposta
 return resposta
 end
 
@@ -111,14 +95,15 @@ end
 ############################################################
 ##
 
-$id = ""
+
 puts "digite a identificaçao do seu PC :"
+$id = ""
 $id = gets
 
 id_path = "./" + $id.chomp + "_new_public.pem"
 id_private_path = "./" + $id.chomp + "_new_private.pem"
 
-if (!(File.exist?(id_path) || File.exist?(id_private_path))) then
+if (!(File.exist?(id_path) or File.exist?(id_private_path))) then
 	
 	new_key = OpenSSL::PKey::RSA.generate( 1024 )
 	# write the new keys as PEM's
@@ -138,19 +123,19 @@ end
 ## Espera por uma conexao e pedido , 
 ## Processa o pedido e o retorna
 
-servidor = TCPServer.open("192.168.160.76" , 8080)   				# Socket to listen on port 
+servidor = TCPServer.open("127.0.0.1" , 8080)   				# Socket to listen on port 
+puts "--------Esperando  conexao --------"
 loop {                          						# Servers run forever
 request,answer = "",""
   Thread.start(servidor.accept) do |client|		
+
 	while line = client.gets   						# Read lines from the socket			
 		if (line == "\r\n") then break end				# until the client prints an empty line  	
 		request << line	
 	end	
 
-	puts request
-	answer = fileProcess(request, chave)
+	answer = fileProcess(request)
 	client.puts(answer) 							# devolve para o cliente o pedido http processado        
-	puts answer
 	client.close 								# Disconnect from the client
   end
 }
--- pedido.rb
+++ pedido.rb
@@ -1,26 +1,56 @@
 require 'socket'
 require 'openssl'
 
-if (ARGV[0] == nil) then
-    puts "erro. digite: ruby pedido.rb 'ip_servidor'\n"
-    exit
+def decrypt(answer,parte_answer)
+	
+	assinatura   = parte_answer[0]
+	key          = parte_answer[1]
+	iv	     = parte_answer[2]
+	chipher_text = parte_answer[3] 
+	hashtext     = parte_answer[4]
+
+	public_file = "./" + $id_retorno.chomp + "_new_public.pem"
+	public_key = OpenSSL::PKey::RSA.new(File.read(public_file))
+	assinatura = public_key.public_decrypt(assinatura)
+
+	if(assinatura != $id_retorno) then 
+		puts "assinatura nao conrresponde\n"
+		#break
+	else	
+		private_key  = OpenSSL::PKey::RSA.new(File.read($id_private_path))
+		key = private_key.private_decrypt(key)
+	end
+
+	c = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+	c.decrypt
+
+	c.key = key
+	c.iv = iv
+	decrypted_text = c.update(chipher_text)
+	decrypted_text << c.final	
+
+	puts "-- Decrypted: #{decrypted_text}\n"
+
+	return decrypted_text,hashtext
 end
 
-socket = TCPSocket.open(ARGV[0], 8080)
+
+
+socket = TCPSocket.open("127.0.0.1", 8080)
 
 $id = ""
 puts "digite a identificaçao do seu PC :"
 $id = gets
 
-id_retorno = ""
+$id_retorno = ""
 puts "digite com id do pc o qual deseja fazer a transferencia:"
-id_retorno = gets
+$id_retorno = gets
 
-id_path = "./" + $id.chomp + "_new_public.pem"
-id_private_path = "./" + $id.chomp + "_new_private.pem"
+$id_path = "./" + $id.chomp + "_new_public.pem"
+$id_private_path = "./" + $id.chomp + "_new_private.pem"
 
 # Testa a existencia da chave publica e privada
-if (!(File.exist?(id_path) || File.exist?(id_private_path) )) then
+if (!(File.exist?($id_path) || File.exist?($id_private_path) )) then
 
 	new_key = OpenSSL::PKey::RSA.generate( 1024 )
 
@@ -29,13 +59,13 @@ if (!(File.exist?(id_path) || File.exist?(id_private_path) )) then
 
 	new_public = new_key.public_key.to_pem
 
-	output_public = File.new(id_path, "w")
+	output_public = File.new($id_path, "w")
 	output_public.puts new_public
 	output_public.close
 
 	new_private = new_key.to_pem
 
-	output_private = File.new(id_private_path, "w")
+	output_private = File.new($id_private_path, "w")
 	output_private.puts new_private
 	output_private.close
 end
@@ -45,39 +75,30 @@ puts "digite o caminho para o arquivo desejado: "
 path = gets
 
 pedido = $id.chomp + "," + path + "\r\n" 
-socket.print(pedido)
-answer = socket.read
+hash = ""
+hashtext = "..."
 
-parte_answer = answer.split("|||||",4)
-assinatura   = parte_answer[0]
-key          = parte_answer[1]
-iv	     = parte_answer[2]
-chipher_text = parte_answer[3] 
+loop{
+
+	socket.print(pedido)
+	answer = socket.read
+
+	parte_answer = answer.split("|||||",5)
+
+	decrypted_text,hashtext = decrypt(answer,parte_answer)
+	hash = Digest::MD5.hexdigest(decrypted_text)				# faz a hash com o texto decriptografado 
+	
+	hashtext.chop!
+	if ( hashtext == hash ) then 
+		puts "Mensagem recebida corretamente"
+		exit 
+
+	end
+	puts "hash nao deu certo"
+	ruu = gets
+}
 
 
-public_file = "./" + id_retorno.chomp + "_new_public.pem"
-public_key = OpenSSL::PKey::RSA.new(File.read(public_file))
-puts assinatura
-assinatura = public_key.public_decrypt(assinatura)
-
-if(assinatura != id_retorno) then 
-	puts "assinatura nao conrresponde\n"
-	#break
-else
-	puts "assinatura ; #{assinatura}"	
-	private_key  = OpenSSL::PKey::RSA.new(File.read(id_private_path))
-	key = private_key.private_decrypt(key)
-	puts "\n\n------------------ " + key
-end
-
-c = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
-c.decrypt
-
-c.key = key
-c.iv = iv
-decrypted_text = c.update(chipher_text)
-puts "-------------------------- " + decrypted_text
-puts "decrypted: #{decrypted_text}\n"
 
 
 
